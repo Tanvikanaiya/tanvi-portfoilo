d958e681b52c4f20ac94b34b3fe9701e95b46ab7
Tanvi kanaiya <136492722+Tanvikanaiya@users.noreply.github.com>
2025-08-25 23:24:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,21 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tanvi Portfolio",
+  description: "Portfolio website of Tanvi Kanaiya",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className="font-sans text-gray-800 antialiased">{children}</body>
+      <body className="font-sans text-gray-800 antialiased">
+        {children}
+      </body>
     </html>
   );
 }
