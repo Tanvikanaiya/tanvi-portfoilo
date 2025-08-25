6dfd942783227f47062f3c4bf8f501e69e09fbbb
Tanvi kanaiya <136492722+Tanvikanaiya@users.noreply.github.com>
2025-08-25 23:02:19 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="font-sans text-gray-800 antialiased">{children}</body>
+    </html>
+  );
+}
